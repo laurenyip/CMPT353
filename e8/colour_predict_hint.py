ac52d574caf2134db0aae789f9accdf3d5adb6d6
laurenyip <laurenyip10@gmail.com>
2024-03-10 23:21:54 -0700

    e8

--- e8/colour_predict_hint.py
+++ e8/colour_predict_hint.py
@@ -1,15 +1,16 @@
-# python3 colour_bayes.py colour-data.csv
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from skimage.color import lab2rgb
 import sys
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import FunctionTransformer
-from skimage.color import rgb2lab
+
+
+OUTPUT_TEMPLATE = (
+    'Bayesian classifier:    {bayes_rgb:.3f} {bayes_convert:.3f}\n'
+    'kNN classifier:         {knn_rgb:.3f} {knn_convert:.3f}\n'
+    'Rand forest classifier: {rf_rgb:.3f} {rf_convert:.3f}\n'
+)
+
 
 # representative RGB colours for each label, for nice display
 COLOUR_RGB = {
@@ -47,7 +48,7 @@ def plot_predictions(model, lum=70, resolution=256):
     X_grid = lab2rgb(lab_grid)
 
     # predict and convert predictions to colours so we can see what's happening
-    y_grid = model.predict(X_grid.reshape((wid*hei, 3)))
+    y_grid = model.predict(X_grid.reshape((-1, 3)))
     pixels = np.stack(name_to_rgb(y_grid), axis=1) / 255
     pixels = pixels.reshape((hei, wid, 3))
 
@@ -60,7 +61,7 @@ def plot_predictions(model, lum=70, resolution=256):
     plt.yticks(np.linspace(0, hei, n_ticks), np.linspace(-100, 100, n_ticks))
     plt.xlabel('A')
     plt.ylabel('B')
-    plt.imshow(X_grid.reshape((hei, wid, 3)))
+    plt.imshow(X_grid.reshape((hei, wid, -1)))
 
     plt.subplot(1, 2, 2)
     plt.title('Predicted Labels')
@@ -70,39 +71,29 @@ def plot_predictions(model, lum=70, resolution=256):
     plt.imshow(pixels)
 
 
-def main(infile):
-    data = pd.read_csv(infile)
-    
-    X = data[['R', 'G', 'B']]/255 # array with shape (n, 3). Divide by 255 so components are all 0-1.
-    y = data['Label'].to_numpy() # array with shape (n,) of colour words.
-    
-    # Add feature names to X
-    X.columns = ['R', 'G', 'B']
+def main():
+    data = pd.read_csv(sys.argv[1])
+    X = data[['R', 'G', 'B']].values / 255
+    y = data['Label'].values
 
-    
-    # TODO: build model_rgb to predict y from X.
-    # partition the data
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
-    
-    # build model_rgb to predict y from X.
-    model_rgb = GaussianNB()
-    model_rgb.fit(X_train, y_train)
-    
-    # TODO: print model_rgb's accuracy score
-    print("1st Accuracy Score: " + str(model_rgb.score(X_test, y_test)))
+    # TODO: create some models
 
-    # build model_lab to predict y from X by converting to LAB colour first.
-    model_lab = make_pipeline(FunctionTransformer(rgb2lab),GaussianNB())
-    model_lab.fit(X_train, y_train)
-    # print model_lab's accuracy score
-    print("2nd Accuracy Score: " + str(model_lab.score(X_test, y_test)))
-    
-    # save images
-    plot_predictions(model_rgb)
-    plt.savefig('predictions_rgb.png')
-    plot_predictions(model_lab)
-    plt.savefig('predictions_lab.png')
+    # train each model and output image of predictions
+    models = [bayes_rgb_model, bayes_convert_model, knn_rgb_model, knn_convert_model, rf_rgb_model, rf_convert_model]
+    for i, m in enumerate(models):  # yes, you can leave this loop in if you want.
+        m.fit(X_train, y_train)
+        plot_predictions(m)
+        plt.savefig('predictions-%i.png' % (i,))
+
+    print(OUTPUT_TEMPLATE.format(
+        bayes_rgb=bayes_rgb_model.score(X_valid, y_valid),
+        bayes_convert=bayes_convert_model.score(X_valid, y_valid),
+        knn_rgb=knn_rgb_model.score(X_valid, y_valid),
+        knn_convert=knn_convert_model.score(X_valid, y_valid),
+        rf_rgb=rf_rgb_model.score(X_valid, y_valid),
+        rf_convert=rf_convert_model.score(X_valid, y_valid),
+    ))
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main()
